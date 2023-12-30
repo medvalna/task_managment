b3a1efb277ae3a8492ac783c2ba83bf24edbf6c6
Medvedeva Valentina <72570836+medvalna@users.noreply.github.com>
2023-12-31 02:13:47 +0300

    Update middleware.ts
    feature: transfer to dashboard page after login

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,20 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
  
 
 export default authMiddleware({
   publicRoutes:["/"],
+  afterAuth(auth, req){
+    if(auth.userId && auth.isPublicRoute){
+      let path = "/dashboard";
+      const dashboard = new URL(path, req.url);
+      return NextResponse.redirect(dashboard);
+    }
+    
+    if (!auth.userId &&   !auth.isPublicRoute){
+      return redirectToSignIn({returnBackUrl: req.url});
+    }
+  }
 });
  
 export const config = {
