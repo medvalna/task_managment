665cd48f863588500fe1885b759313e21eb042ac
Medvedeva Valentina <72570836+medvalna@users.noreply.github.com>
2024-01-09 12:19:48 +0300

    fix: added button under table

--- app/(platform)/dashboard/[userId]/_components/todoList.tsx
+++ app/(platform)/dashboard/[userId]/_components/todoList.tsx
@@ -3,6 +3,7 @@ import { Poppins } from "next/font/google";
 
 import { cn } from "@/lib/utils";
 import Task from "./task";
+import Modal from "./modal";
 
 const headingFont = Poppins({
   subsets: ["latin"],
@@ -13,26 +14,41 @@ interface TodoListProps {
 }
 const TodoList: React.FC<TodoListProps> = ({ tasks }) => {
   return (
-    <div className="">
-      <table className="table w-auto">
-        {/* head */}
-        <thead>
-          <tr>
-            <th className={cn("text-center text-lg", headingFont.className)}>
-              Task
-            </th>
-            <th className={cn("text-center", headingFont.className)}>
-              Actions
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {(Array.isArray(tasks) && tasks.length) ? tasks.map((task) => (
-            <Task key={task.id} task={task} />
-          )): null}
-        </tbody>
-      </table>
-    </div>
+    <>
+      <div className="flex items-left gap-6 mx-24 mt-12 mb-5">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th
+                className={cn(
+                  "text-left text-lg w-auto",
+                  headingFont.className
+                )}
+              >
+                Task
+              </th>
+              <th className={cn("text-left", headingFont.className)}>
+                Actions
+              </th>
+            </tr>
+            
+          </thead>
+          
+          <tbody>
+            {Array.isArray(tasks) && tasks.length
+              ? tasks.map((task) => <Task key={task.id} task={task} />)
+              : null}
+          </tbody>
+          
+        </table>
+        
+      </div>
+      <div className="border-b-2 border-gray-300 mr-60 ml-24"></div>
+      <div className="float-right mr-60">
+        <Modal />
+      </div>
+    </>
   );
 };
 
