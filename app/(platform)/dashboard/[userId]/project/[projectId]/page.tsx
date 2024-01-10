94d1a30a75dcdf6d2ff5657205ea44aa9c92bf0d
Medvedeva Valentina <72570836+medvalna@users.noreply.github.com>
2024-01-10 16:51:37 +0300

    feat: added project page

--- app/(platform)/dashboard/[userId]/project/[projectId]/page.tsx
+++ app/(platform)/dashboard/[userId]/project/[projectId]/page.tsx
@@ -1,9 +1,31 @@
-const ProjectPage = () => {
-    return (
-        <div>
-            New Project
+import { getAllTodos } from "@/app/(api)/apiTasks";
+import Modal from "../../_components/modal";
+import TodoList from "../../_components/todoList";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { useRouter, useSearchParams } from "next/navigation";
+
+const headingFont = Poppins({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+const ProjectPage = async () => {
+  
+  const tasks = await getAllTodos(`sport`);
+  //const router = useRouter();
+ 
+  return (
+    <div className=" bg-violet-50 h-screen w-screen">
+      <div className="text-left my-5 mx-5 flex space-x-2">
+        <div className={cn("text-2xl text-violet-950", headingFont.className)}>
+          Sport
         </div>
-    );
-}
+        <Modal />
+      </div>
+      <TodoList tasks={tasks} />
+    </div>
+  );
+};
 
 export default ProjectPage;
