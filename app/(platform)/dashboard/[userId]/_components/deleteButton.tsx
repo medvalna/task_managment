65627461b4cfc1630f2115aca6bf452e7116b1ea
Medvedeva Valentina <72570836+medvalna@users.noreply.github.com>
2024-01-13 13:43:18 +0300

    feat: added delete button for project

---
+++ app/(platform)/dashboard/[userId]/_components/deleteButton.tsx
@@ -0,0 +1,25 @@
+'use client'
+import { IoTrashBinOutline } from "react-icons/io5";
+import { Button } from "@/components/ui/button";
+import { deleteProject } from "@/app/(api)/apiProjects";
+import { MouseEventHandler } from "react";
+import { useRouter } from "next/navigation";
+interface DeleteButtonProps{
+    projectId: string;
+    projectName: string;
+}
+const DeleteButton: React.FC<DeleteButtonProps> = ({ projectId,projectName}) => {
+    const router = useRouter();
+    const handleDeleteProjButton: MouseEventHandler<HTMLButtonElement> = async (
+        e
+      ) => {
+        e.preventDefault();
+        router.push('/')
+        await deleteProject(projectId, projectName);
+        
+      };
+    return(<IoTrashBinOutline className = "rounded-lg hover:bg-violet-300 py-3 w-10 h-10" onClick={handleDeleteProjButton} />);
+
+}
+
+export default DeleteButton;
