b8c2b6773ef050bb0ce7cb1e717f22eecc3518cd
Medvedeva Valentina <72570836+medvalna@users.noreply.github.com>
2024-01-13 23:40:26 +0300

    fix: trash icon

--- app/(platform)/dashboard/[userId]/_components/deleteButton.tsx
+++ app/(platform)/dashboard/[userId]/_components/deleteButton.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { deleteProject } from "@/app/(api)/apiProjects";
 import { MouseEventHandler } from "react";
 import { useRouter } from "next/navigation";
+import { FaRegTrashAlt } from "react-icons/fa";
 interface DeleteButtonProps{
     projectId: string;
     projectName: string;
@@ -18,7 +19,7 @@ const DeleteButton: React.FC<DeleteButtonProps> = ({ projectId,projectName}) => 
         await deleteProject(projectId, projectName);
         router.refresh();
       };
-    return(<IoTrashBinOutline className = "rounded-lg hover:bg-violet-300 py-3 w-10 h-10" onClick={handleDeleteProjButton} />);
+    return(<FaRegTrashAlt className = "rounded-lg hover:bg-violet-300 text-violet-950 py-3 w-10 h-10" onClick={handleDeleteProjButton} />);
 
 }
 
